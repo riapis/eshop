b0918e6f17e48e2e42dde672740284ab5390e11d
riapis <fari.hafizh@ui.ac.id>
2024-02-07 01:09:51 +0700

    update controller for delete-product

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -34,4 +34,11 @@ public class ProductController {
         model.addAttribute("products", allProducts);
         return "ProductList";
     }
+
+    @PostMapping("/delete/{productId}")
+    public String deleteProductPost(@PathVariable String productId){
+        Product product = service.findById(productId);
+        service.delete(product);
+        return "redirect:/product/list";
+    }
 }
