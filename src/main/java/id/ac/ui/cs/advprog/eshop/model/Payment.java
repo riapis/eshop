2dee111c9b1fc71338a32e50a0e3273e180c4acd
riapis <fari.hafizh@ui.ac.id>
2024-03-10 23:13:05 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -15,7 +15,26 @@ public class Payment {
     private String status;
 
     public Payment(String id, String method, Order order, Map<String, String> paymentData){
+        this.id = id;
+        setStatus("SUCCESS");
 
+        if (method.equals("VoucherCode") || method.equals("CashOnDelivery")){
+            this.method = method;
+        }else {
+            throw new IllegalArgumentException();
+        }
+
+        if(paymentData.isEmpty()){
+            throw new IllegalArgumentException();
+        }else {
+            this.paymentData = paymentData;
+        }
+
+        if(order == null){
+            throw new IllegalArgumentException();
+        }else {
+            this.order = order;
+        }
     }
 
 }
